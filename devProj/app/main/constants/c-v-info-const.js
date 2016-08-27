6dba557523bc1504896ca37db517d414055058f5
Ashraf Abd Elsatar <ashraf.elkoly@gmail.com>
2016-08-27 15:59:16 +0200

    add the whole project with the build v0.1

---
+++ devProj/app/main/constants/c-v-info-const.js
@@ -0,0 +1,31 @@
+'use strict';
+angular.module('main').constant('CVInfo', {
+	'personalInfo': {
+		'name': 'Ashraf Abd Elsatar',
+		'mobile': '(+20)-122-789-8935',
+		'email': 'ashraf.elkoly@gmail.com',
+		'street': '43 khalil abu yossef',
+		'floor': '6',
+		'zone': 'Faisal, Giza, Egypt',
+		'postal': '12111',
+	},
+	'skills': [
+		{
+			'name': 'Java/EE',
+			'score': '70'
+		},
+		{
+			'name': 'Javascript',
+			'score': '90'
+		},
+		{
+			'name': 'AngularJs',
+			'score': '90'
+		},
+		{
+			'name': 'NodeJs',
+			'score': '90'
+		},
+
+	]
+});
